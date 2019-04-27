47c5cab23d866d402431791c88ece8dd9d0ab877
SERGEYZHESTKOV\Sergey <serezha.zhestkov95@gmail.com>
2019-04-27 19:36:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,23 +18,24 @@ LANGUAGES = [
 ]
 
 
-def find_vacancies_develop_by_language_hh(language):
+def find_vacancies_develop_by_language_hh(language, count_days = 30, area_msk = 1):
     temp_page = 0
     pages_number =1 
+    page_max = 50 
     vacancies = []
     while temp_page <= pages_number:
         url = 'https://api.hh.ru/vacancies'
-        payload = {"text":"программист {}".format(language), "area":"1", "period":'30', 'page':temp_page}
+        payload = {"text":"программист {}".format(language), "area":area_msk, "period":count_days, 'page':temp_page}
         response = requests.get(url,  params = payload)
         result = response.json()
+        response.raise_for_status()
         pages_number = result['pages']
-        if pages_number > 50:
-            pages_number = 50
+        if pages_number > page_max:
+            pages_number = page_max
         temp_page +=1 
         for vacancy in result['items']:
             vacancies.append(vacancy)
-  
-
+    print("hh", len(vacancies))
     return vacancies
       
    
@@ -42,20 +43,21 @@ def predict_rub_salary_hh(vacancy):
     if vacancy['salary']:
         salary = vacancy['salary']
         if salary['currency'] != "RUR":
-            return None
+            return
         return predict_salary(salary['from'], salary['to'])
     else:
-        return None
+        return
 
 
-def find_vacancies_develop_by_language_sj(language, token):
+def find_vacancies_develop_by_language_sj(language, token, catalogues = 48, town = 4):
     vacancies = []
     page = 0
     while True:
         url = 'https://api.superjob.ru/2.0/vacancies/'
         headers = {'X-Api-App-Id': token}
-        payload = {"catalogues":48, "town":4, "keyword":language, 'page':page}
+        payload = {"catalogues":catalogues, "town":town, "keyword":language, 'page':page}
         response = requests.get(url,  headers = headers, params = payload)
+        response.raise_for_status()
         result = response.json()
         for vacancy in result['objects']:
             vacancies.append(vacancy)
@@ -63,13 +65,13 @@ def find_vacancies_develop_by_language_sj(language, token):
             page += 1
         else:
             break
-
+    print("sj", len(vacancies))
     return vacancies
 
 
 def predict_salary(salary_from, salary_to):
     if not salary_from and not salary_to:
-        return None
+        return 
     if salary_from and salary_to:
         return (salary_from + salary_to) / 2
     if not salary_from:
@@ -80,7 +82,7 @@ def predict_salary(salary_from, salary_to):
 
 def predict_rub_salary_sj(vacancy):
     if vacancy['currency'] != "rub":
-        return None
+        return
     return predict_salary(vacancy['payment_from'], vacancy['payment_to'])
  
 
@@ -132,14 +134,28 @@ def get_info_sj(languages, token):
             'average_salary' : average_salary,
             'vacancies_processed' : vacancies_processed,
         }
-    return info_from_hh        
+    return info_from_sj       
      
 
 if __name__ == "__main__":
     load_dotenv()
     token_sj = os.getenv("TOKEN_SJ")
-    info_from_hh = get_info_hh(LANGUAGES)
-    info_from_sj = get_info_sj(LANGUAGES, token_sj)
+    success_hh = True
+    success_sj = True
+    
+    try:
+        info_from_hh = get_info_hh(LANGUAGES)
+    except  requests.exceptions.HTTPError as err:
+        print("Error request from HeadHunter:", err)  
+        success_hh = False  
+        
+    try:
+        info_from_sj = get_info_sj(LANGUAGES, token_sj)
+    except  requests.exceptions.HTTPError as err:
+        print("Error request from SuperJob:", err)  
+        success_sj = False
 
-    print_table(info_from_sj, "SuperJob")
-    print_table(info_from_hh, "HeadHunter")
+    if success_sj:
+        print_table(info_from_sj, "SuperJob")
+    if success_hh:
+        print_table(info_from_hh, "HeadHunter")
